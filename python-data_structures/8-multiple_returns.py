6c5b44d7b9e1525df7a2e1f7e486094981a76030
hngabo-lang <h.ngabo@alustudent.com>
2026-03-10 12:02:24 -0700

    gh

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Returns tuple of length and first character of a string."""
+
+
 def multiple_returns(sentence):
+    """Return (length, first_char) or (0, None) if empty."""
     if len(sentence) == 0:
         return (0, None)
     return (len(sentence), sentence[0])
