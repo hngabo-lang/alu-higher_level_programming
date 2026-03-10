57dfe2bfbec53aefa86e0def9e5a20350a015b88
hngabo-lang <h.ngabo@alustudent.com>
2026-03-10 11:47:13 -0700

    del

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Deletes the item at a specific position in a list."""
+
+
+
 def delete_at(my_list=[], idx=0):
+     """Return list with element at idx removed."""
     if idx < 0 or idx >= len(my_list):
         return my_list
-    del my_list[idx]   # not pop()!
+    del my_list[idx]   
     return my_list
